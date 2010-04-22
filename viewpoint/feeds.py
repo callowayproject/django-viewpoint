2c774838d438d9ce52cf2512ffe1b741785b787b
Corey Oordt <coordt@washingtontimes.com>
2010-04-22 09:29:21 -0400

    Removed some of the feeds since they assumed a depndency (mptt) and made the categories optional only if it was installed

--- viewpoint/feeds.py
+++ viewpoint/feeds.py
@@ -1,48 +1,50 @@
 from datetime import datetime, timedelta
 from django.contrib.syndication.feeds import Feed
-from categories.models import Category
+from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
+
 from models import Entry, Blog
 
-from mptt_comments.models import MpttComment
-from django.contrib.contenttypes.models import ContentType
 
 ENTRY_CONTENT_TYPE = ContentType.objects.get_for_model(Entry)
 
-class EntryComments(Feed):
-    def get_object(self, bits):
-        if len(bits) < 1:
-            return Blog.objects.all()[0]
-        return Blog.objects.get(slug__iexact=bits[-1], public=True)
-    
-    def title(self, obj):
-        return "Neighborhood comments for '%s'" % obj.title
-        
-    def link(self, obj):
-        return obj.get_absolute_url()
-    
-    def description(self, obj):
-        return "Latest entry comments posted for neighborhood %s" % obj.title
-        
-    def items(self, obj):
-        nids = []
-        for i in Entry.objects.published().filter(blog__pk=obj.pk).values('pk'):
-            for k,v in i.items():
-                nids.append(str(v))
-                
-        return MpttComment.objects.filter(is_public=True, is_removed=False, 
-            content_type__pk=ENTRY_CONTENT_TYPE.pk, 
-            object_pk__in=nids).exclude(parent=None).order_by('-submit_date')[:15]
-        
-    def item_link(self, item):
-        return item.content_object.get_absolute_url()
-        
-    def item_guid(self, item):
-        return item.get_absolute_url()
-        
-    def item_pubdate(self, item):
-        return item.submit_date
-        
-        
+# class EntryComments(Feed):
+#     def get_object(self, bits):
+#         if len(bits) < 1:
+#             return Blog.objects.all()[0]
+#         return Blog.objects.get(slug__iexact=bits[-1], public=True)
+#     
+#     def title(self, obj):
+#         return "Neighborhood comments for '%s'" % obj.title
+#         
+#     def link(self, obj):
+#         return obj.get_absolute_url()
+#     
+#     def description(self, obj):
+#         return "Latest entry comments posted for neighborhood %s" % obj.title
+#         
+#     def items(self, obj):
+#         nids = []
+#         for i in Entry.objects.published().filter(blog__pk=obj.pk).values('pk'):
+#             for k,v in i.items():
+#                 nids.append(str(v))
+#         
+#         from mptt_comments.models import MpttComment
+#         
+#         return MpttComment.objects.filter(is_public=True, is_removed=False, 
+#             content_type__pk=ENTRY_CONTENT_TYPE.pk, 
+#             object_pk__in=nids).exclude(parent=None).order_by('-submit_date')[:15]
+#         
+#     def item_link(self, item):
+#         return item.content_object.get_absolute_url()
+#         
+#     def item_guid(self, item):
+#         return item.get_absolute_url()
+#         
+#     def item_pubdate(self, item):
+#         return item.submit_date
+
+
 class LatestEntries(Feed):
     title = "Latest entires from our community"
     link = "/neighborhood/"
@@ -57,26 +59,29 @@ class LatestEntries(Feed):
         t = item.pub_time
         dt = datetime(d.year, d.month, d.day, t.hour, t.minute, t.second)
         return dt
-       
-class LatestEntriesByCategory(Feed):
 
-    def get_object(self, bits):
-        if len(bits) < 1:
-            return Category.objects.all()[0]
-        return Category.objects.get(slug=bits[-1])
+if 'categories' in settings.INSTALLED_APPS:
+    from categories.models import Category
+    
+    class LatestEntriesByCategory(Feed):
 
-    def title(self, obj):
-        return "My blog for category '%s'" % obj.name
+        def get_object(self, bits):
+            if len(bits) < 1:
+                return Category.objects.all()[0]
+            return Category.objects.get(slug=bits[-1])
 
-    def link(self, obj):
-        return obj.get_absolute_url()
+        def title(self, obj):
+            return "My blog for category '%s'" % obj.name
 
-    def description(self, obj):
-        return "Blog entries recently posted in category %s" % obj.name
+        def link(self, obj):
+            return obj.get_absolute_url()
 
-    def items(self, obj):
-        return Entry.objects.published(category__slug=obj.slug).order_by('-pub_date')[:10]
-        
+        def description(self, obj):
+            return "Blog entries recently posted in category %s" % obj.name
+
+        def items(self, obj):
+            return Entry.objects.published(category__slug=obj.slug).order_by('-pub_date')[:10]
+
 
 class LatestEntriesByBlog(Feed):
     def get_object(self, bits):
