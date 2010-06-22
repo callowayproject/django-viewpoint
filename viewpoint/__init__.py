2fa2ba2b83af515d8e4d073e47c2282684eb9a11
Corey Oordt <coordt@washingtontimes.com>
2010-06-22 08:52:45 -0400

    Bumped the version to beta for testing

--- viewpoint/__init__.py
+++ viewpoint/__init__.py
@@ -2,8 +2,8 @@ __version_info__ = {
     'major': 0,
     'minor': 6,
     'micro': 6,
-    'releaselevel': 'final',
-    'serial': 0
+    'releaselevel': 'beta',
+    'serial': 1
 }
 
 def get_version():
