13d528464e3299867a499dc772e3fe5f7a9cdb46
Corey Oordt <coordt@washingtontimes.com>
2010-10-25 08:04:30 -0400

    Added new settings.
    USE_CATEGORIES, AUTHOR_MODEL, USE_TAGGING, DEFAULT_BLOG
    are added. The author model setting allows for
    any model to be used instead of staff. The default
    blog allows for a specific blog to be assumed.

--- viewpoint/settings.py
+++ viewpoint/settings.py
@@ -9,3 +9,9 @@ BLOG_RELATION_MODELS = getattr(settings, 'BLOG_RELATION_MODELS', [])
 USE_APPROVAL = getattr(settings, 'VIEWPOINT_USE_APPROVAL', False)
 
 DEFAULT_STORAGE = getattr(settings, 'VIEWPOINT_DEFAULT_STORAGE', settings.DEFAULT_FILE_STORAGE)
+
+USE_CATEGORIES = getattr(settings, 'VIEWPOINT_USE_CATEGORIES', False)
+AUTHOR_MODEL = getattr(settings, 'VIEWPOINT_AUTHOR_MODEL', 'auth.user')
+USE_TAGGING = getattr(settings, 'VIEWPOINT_USE_TAGGING', False)
+
+DEFAULT_BLOG = getattr(settings, 'VIEWPOINT_DEFAULT_BLOG', '')
