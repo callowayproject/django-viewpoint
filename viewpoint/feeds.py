8f72506af63a1038dd0f174ec7257bb483ff31f4
Corey Oordt <coordt@washingtontimes.com>
2010-11-03 08:58:25 -0400

    Added a link attribute and a new version of the feed classes for 1.2

--- viewpoint/feeds.py
+++ viewpoint/feeds.py
@@ -1,21 +1,37 @@
 from datetime import datetime, timedelta
 from django.contrib.syndication.feeds import Feed
+from django.contrib.syndication.views import Feed as FeedView
 from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
 from django.contrib.sites.models import Site
-from viewpoint.settings import USE_CATEGORIES
+from django.core.urlresolvers import reverse, resolve
+from viewpoint.settings import USE_CATEGORIES, DEFAULT_BLOG
 
 from models import Entry, Blog
 
 ENTRY_CONTENT_TYPE = ContentType.objects.get_for_model(Entry)
 
 class LatestEntries(Feed):
-    title = "Entries from the last week on %s" % Site.objects.get_current().name
-    description = "Entries from the last week on %s" % Site.objects.get_current().name
+    title = "Latest entries on %s" % Site.objects.get_current().name
+    description = "Latest entries on %s" % Site.objects.get_current().name
+    link = "/"
     
     def items(self):
-        start = datetime.now() - timedelta(days=7)
-        return Entry.objects.published().filter(pub_date__gte=start).order_by('-pub_date')
+        return Entry.objects.published().order_by('-pub_date')[:15]
+        
+    def item_pubdate(self, item):
+        d = item.pub_date
+        t = item.pub_time
+        dt = datetime(d.year, d.month, d.day, t.hour, t.minute, t.second)
+        return dt
+
+class LatestEntriesView(FeedView):
+    title = "Latest entries on %s" % Site.objects.get_current().name
+    description = "Latest entries on %s" % Site.objects.get_current().name
+    link = '/'
+    
+    def items(self):
+        return Entry.objects.published().order_by('-pub_date')[:15]
         
     def item_pubdate(self, item):
         d = item.pub_date
@@ -48,9 +64,39 @@ if USE_CATEGORIES and 'categories' in settings.INSTALLED_APPS:
 
 class LatestEntriesByBlog(Feed):
     def get_object(self, bits):
-        if len(bits) < 1:
+        if len(bits) < 1 and DEFAULT_BLOG == '':
             return Blog.objects.all()[0]
-        return Blog.objects.get(slug__iexact=bits[-1], public=True)
+        if DEFAULT_BLOG != '':
+            return Blog.objects.get(slug__iexact=DEFAULT_BLOG, public=True)
+        else:
+            return Blog.objects.get(slug__iexact=bits[-1], public=True)
+        
+    def title(self, obj):
+        return "Latest entries for %s" % obj.title
+        
+    def link(self, obj):
+        return obj.get_absolute_url()
+        
+    def description(self, obj):
+        return "Latest entries posted for %s" % obj.title
+        
+    def items(self, obj):
+        return Entry.objects.published(blog__pk=obj.pk).order_by('-pub_date')[:15]
+        
+    def item_pubdate(self, item):
+        d = item.pub_date
+        t = item.pub_time
+        dt = datetime(d.year, d.month, d.day, t.hour, t.minute, t.second)
+        return dt
+
+class LatestEntriesByBlogView(Feed):
+    def get_object(self, bits):
+        if len(bits) < 1 and DEFAULT_BLOG == '':
+            return Blog.objects.all()[0]
+        if DEFAULT_BLOG != '':
+            return Blog.objects.get(slug__iexact=DEFAULT_BLOG, public=True)
+        else:
+            return Blog.objects.get(slug__iexact=bits[-1], public=True)
         
     def title(self, obj):
         return "Latest entries for %s" % obj.title
