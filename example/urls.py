4a521dc5aafbcd9aad98ac7e8fed4f7ffdd4816f
jose soares <jsoares@jose-mac.nwc.newsworld.net>
2010-11-04 11:48:43 -0400

    Modified feeds so they can be rss or atom

--- example/urls.py
+++ example/urls.py
@@ -4,7 +4,16 @@ from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
-from viewpoint.feeds import LatestEntriesView
+from viewpoint.feeds import LatestEntriesView, LatestEntries, LatestEntriesByBlog
+
+rss_feeds = {
+    'latest': LatestEntries(feed_type='rss'),
+    'blog': LatestEntriesByBlog(feed_type='rss')
+}
+atom_feeds = {
+    'latest': LatestEntries(feed_type='atom'),
+    'blog': LatestEntriesByBlog(feed_type='atom')
+}
 
 urlpatterns = patterns('',
     # Uncomment for Multiple blogs
@@ -12,6 +21,9 @@ urlpatterns = patterns('',
     #Uncomment for One blog
     (r'^blogs/', include('viewpoint.urls')),
     (r'^feeds/all/$', LatestEntriesView()),
+    
+    (r'^rss/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': rss_feeds}),
+    (r'^atom/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': atom_feeds}),
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
