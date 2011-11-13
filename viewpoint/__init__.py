12376dcb50107899cd1e42d874ebedef327510fa
Corey Oordt <coreyoordt@gmail.com>
2011-11-13 03:29:25 -0500

    Version bump to 0.9b1

--- viewpoint/__init__.py
+++ viewpoint/__init__.py
@@ -1,8 +1,8 @@
 __version_info__ = {
     'major': 0,
-    'minor': 8,
-    'micro': 7,
-    'releaselevel': 'final',
+    'minor': 9,
+    'micro': 0,
+    'releaselevel': 'beta',
     'serial': 1
 }
 
